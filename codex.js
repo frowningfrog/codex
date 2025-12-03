443082dd5e7214ade9a82c3d309a7c920acfc762
frowningfrog <natalizedstolpinator@hotmail.com>
2025-12-03 00:32:39 -0700

    initialize canvas

---
+++ codex.js
@@ -0,0 +1,8 @@
+function start() {
+    const canvas = document.getElementById("canvas");
+    canvas.width = window.innerWidth;   canvas.height = window.innerHeight;
+    ctx = canvas.getContext("2d");
+    ctx.imageSmoothingEnabled = false;  // not fuzzy yay
+    const resize = canvas.width/360;
+    ctx.scale(resize, resize);      // scales to window nicely
+}
